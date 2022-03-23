68ec8eb563b9029a4d1b689526aa2ac348e6db32
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-03-23 15:51:50 +0900

    [레트로핏 인터페이스]

--- solution/app/src/main/java/test/map/dak_project/retrofit/Retrofit_InterFace.kt
+++ solution/app/src/main/java/test/map/dak_project/retrofit/Retrofit_InterFace.kt
@@ -10,7 +10,7 @@ import test.map.dak_project.dakmodel_Package.Item
 import test.map.dak_project.dakmodel_Package.ItemConsult
 
 
-//레트로핏 인터페이스
+//[레트로핏 인터페이스]
 interface Retrofit_InterFace {
 
     @GET("Docfriends_Android_Recruit/api/home.json")
